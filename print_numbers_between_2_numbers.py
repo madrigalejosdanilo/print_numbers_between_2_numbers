5b0a04e7f62b529c926ac1c70d16afead1d136aa
Danilo Madrigalejos <danilo.madrigalejos@vipre.com>
2025-03-08 16:02:22 +0800

    print output

--- print_numbers_between_2_numbers.py
+++ print_numbers_between_2_numbers.py
@@ -2,5 +2,6 @@
 num1 = int(input("Please input 1st number: "))
 num2 = int(input("Please input 2nd number: "))
 
-
 # print next to first number , before 2nd number
+for number in range(num1 + 1, num2):
+    print(number)
